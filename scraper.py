641fde319c5180dfd2a33c2dc9767b03accbe734
Hidde Jessen <=>
2019-06-11 10:06:18 +0200

    start with scraper

--- scraper.py
+++ scraper.py
@@ -12,13 +12,23 @@ terms = ['drone detection %s'%term.rstrip("\n\r") for term in termfile]
 links = []
 
 for term in terms:
-	print(term)
-	for j in search(term, tld="co.in", num=10, stop=1, pause=0.1): 
-		print(j)
-		links.append(j)
+	print('term', term)
+	for url in search(term, tld="co.in", num=10, stop=10, pause=0.1):
+		links.append(url)
 
-for url in links:
-	html = urllib.request.urlopen(url).read()
-	soup = BeautifulSoup(html)
-	text = soup.get_text()
-	print(text)
+for i in range(len(links)):
+	try:
+		url = links[i]
+
+		file = open('%s.txt'%i, 'w')
+		file.write('This information is from %s\n\n'%url)
+		html = urllib.request.urlopen(url).read()
+		soup = BeautifulSoup(html)
+		[s.extract() for s in soup('script')]
+		[s.extract() for s in soup('style')]
+		text = soup.get_text()
+		# lines = [line.strip() for line in text.splitlines()]
+		file.write(text)
+		file.close()
+	except:
+		print('noop')
