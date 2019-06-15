5a750d2c5c405c730e0a4846d7728e8c946ccec1
Jardenna Mohazzab <jardenna.nl@gmail.com>
2019-06-15 22:08:05 +0200

    phase classifier with cosine similarity update

--- phase_classifier_with_cosine.py
+++ phase_classifier_with_cosine.py
@@ -41,8 +41,6 @@ def determine_terms():
 terms = determine_terms()
 
 
-
-
 def phase_classifier(url, terms):
     """ 
         Located keywords in the text of a webpage.
@@ -56,8 +54,9 @@ def phase_classifier(url, terms):
     # Make dictionary with key the word and as value the surrounding keywords and their frequency.
     freq_sur_keyw = freq_sur_keywords(indices, search_terms, text)
     # Determine most_likely_phase for a word at a certain index
-    most_likely_phase = most_likely_phases(indices, freq_sur_keyw, terms, text)
+    most_likely_phase = most_likely_phases(indices, terms, text)
     return most_likely_phase
+
 
 def strip_punctuation(text):
     return ''.join(word for word in text if word not in punctuation)
@@ -65,21 +64,16 @@ def strip_punctuation(text):
 def find_index_of_term(search_terms, text):
     return [i for i, word in enumerate(text) for term in search_terms if word == term]
 
+
 def freq_sur_keywords(indices, search_terms, text):
-    """
-        Make dictionary of the frequency of surrounding keywords with as key the center word and
-        ans value a dictionary with the surrounding keywords and their frequency.
-    """
     sur_word_freq = {}
     for i in indices:
         sur_text = surrounding_text(i, text, selection=700)
         sur_word_freq[text[i]] = freq_of_words(sur_text, search_terms)
     return sur_word_freq
 
+
 def surrounding_text(index, text, selection):
-    """
-        Get surrounding text of a word given its index.
-    """
     range_1 = index - selection
     range_2 = index + selection + 1
     if range_1 >= 0 and range_2 <= len(text):
@@ -91,10 +85,8 @@ def surrounding_text(index, text, selection):
     else:
         return text[0:len(text)]
 
+
 def freq_of_words(sur_text, search_terms):
-    """
-        Determine the frequency of a word and store in a dictionary.
-    """
     overlapping_words = set(sur_text) & set(search_terms)
     word_freqs = Counter(sur_text)
     freq_dict = {}
@@ -103,41 +95,27 @@ def freq_of_words(sur_text, search_terms):
     return freq_dict
 
 
-
-def most_likely_phases(indices, freq_sur_keyw, terms, text):
-    likelyhoods_for_word = {}
-    for i in indices:
-        phase = determine_likelyhood(freq_sur_keyw, terms)
-        max_phase = get_maximum(phase)
-        likelyhoods_for_word[(text[i], i)] = max_phase
-    return likelyhoods_for_word
-
-
-def determine_likelyhood(freq_sur_keyw, terms):
+def determine_likelyhood(i, text, terms):
     """
         Determines the most likely phase that a keyword found in the text belongs to based on the
         surrounding keywords found in the surroudning text.
     """
     score = {}
-    for center_word in freq_sur_keyw:
-        sur_key_list = [key for key in freq_sur_keyw[center_word]]
-        for term in terms:
-            phase_keys = terms[term]
-            overlap = set(sur_key_list) & set(phase_keys)
-            score[term] = phase_score(overlap, freq_sur_keyw, center_word, phase_keys)
+    for term in terms:
+        sur_text = surrounding_text(i, text, selection=700)
+        sur_text_vec = Counter(sur_text)
+        term_vect = term_vector(terms[term])
+        score[term] = determine_cosine(sur_text_vec, term_vect)
     return score
 
-
-def phase_score(overlap, freq_sur_keyw, center_word, phase_keys):
-    """
-        Determines the ratio of the amount of overlapping keywords and the keywords that 
-        determines 
-    """
-    count = 0
-    for word in overlap:
-        count += freq_sur_keyw[center_word][word]
-    return count / len(phase_keys)
-
+def most_likely_phases(indices, terms, text):
+    likelyhoods_for_word = {}
+    for i in indices:
+        phase = determine_likelyhood(i, text, terms)
+        max_phase = get_maximum(phase)
+        likelyhoods_for_word[(text[i], i)] = max_phase
+    return likelyhoods_for_word
+    
 
 def get_maximum(phase):
     maximum = max(phase)
