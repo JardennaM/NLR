d1312ce03957bab2ef6bc3c62be669a1fa124cd3
Jardenna Mohazzab <jardenna.nl@gmail.com>
2019-06-18 14:43:48 +0200

    phase_classifier

--- phase_classifier_word_2_vec.py
+++ phase_classifier_word_2_vec.py
@@ -71,6 +71,8 @@ terms = getTerms()
 
 
 
+
+
 # Read the data into a list of strings.
 def read_data(filename):
     """Extract the first file as a list of words."""
@@ -134,6 +136,30 @@ def generate_batch(data, batch_size, num_skips, skip_window):
 
 
 
+
+
+
+
+
+
+words = read_data('../pages/classification/camera (uv)_2.txt')
+
+terms = getTerms()
+
+vocabulary_size = 10
+
+data, count, dictionary, reversed_dictionary = build_dataset(words, vocabulary_size)
+
+batch, context = generate_batch(data, batch_size=5, num_skips=1, skip_window=2)
+
+print(context)
+
+train_context = context
+
+
+
+
+
 # We pick a random validation set to sample nearest neighbors. Here we limit the
 # validation samples to the words that have a low numeric ID, which by
 # construction are also the most frequent.
@@ -142,14 +168,19 @@ valid_window = 100  # Only pick dev samples in the head of the distribution.
 valid_examples = np.random.choice(valid_window, valid_size, replace=False)
 
 
-batch_size = 128
-embedding_size = 128  # Dimension of the embedding vector.
-skip_window = 1       # How many words to consider left and right.
+batch_size = 5
+embedding_size = 5  # Dimension of the embedding vector.
+skip_window = 64       # How many words to consider left and right.
 num_skips = 2         # How many times to reuse an input to generate a context.
 
 train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
 train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
 valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+# vocabulary_size = 7
+# embedding_size = 3
+
+
 
 
 # Look up embeddings for inputs.
@@ -183,12 +214,60 @@ similarity = tf.matmul(
 
 
 
+with tf.Session(graph=graph) as session:
+  # We must initialize all variables before we use them.
+  init.run()
+  print('Initialized')
 
-words = read_data('../pages/classification/camera (uv)_2.txt')
+  average_loss = 0
+  for step in range(num_steps):
+    batch_inputs, batch_context = generate_batch(data,
+        batch_size, num_skips, skip_window)
+    feed_dict = {train_inputs: batch_inputs, train_context: batch_context}
 
-terms = getTerms()
+    # We perform one update step by evaluating the optimizer op (including it
+    # in the list of returned values for session.run()
+    _, loss_val = session.run([optimizer, cross_entropy], feed_dict=feed_dict)
+    average_loss += loss_val
 
-data, count, dictionary, reversed_dictionary = build_dataset(words, 1000)
+    if step % 2000 == 0:
+      if step > 0:
+        average_loss /= 2000
+      # The average loss is an estimate of the loss over the last 2000 batches.
+      print('Average loss at step ', step, ': ', average_loss)
+      average_loss = 0
 
-batch, context = generate_batch(data, batch_size=5, num_skips=1, skip_window=2)
+
+# Note that this is expensive (~20% slowdown if computed every 500 steps)
+if step % 10000 == 0:
+    sim = similarity.eval()
+    for i in range(valid_size):
+        valid_word = reverse_dictionary[valid_examples[i]]
+        top_k = 8  # number of nearest neighbors
+        nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+        log_str = 'Nearest to %s:' % valid_word
+        for k in range(top_k):
+            close_word = reverse_dictionary[nearest[k]]
+            log_str = '%s %s,' % (log_str, close_word)
+        print(log_str)
+
+final_embeddings = normalized_embeddings.eval()
+
+
+# # Construct the variables for the NCE loss
+# nce_weights = tf.Variable(
+#         tf.truncated_normal([vocabulary_size, embedding_size],
+#                             stddev=1.0 / math.sqrt(embedding_size)))
+# nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+# nce_loss = tf.reduce_mean(
+#         tf.nn.nce_loss(weights=nce_weights,
+#                        biases=nce_biases,
+#                        labels=train_context,
+#                        inputs=embed,
+#                        num_sampled=num_sampled,
+#                        num_classes=vocabulary_size))
+
+# optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(nce_loss)
+
 
